48bee1fa700e8b7bfd4f2b8083041e46e945c4f6
seyoung <designseyoung@gmail.com>
2018-02-06 20:20:46 -0500

    Making unlock screen, by followig existig example

--- Unlock/Unlock/UnlockViewController.swift
+++ Unlock/Unlock/UnlockViewController.swift
@@ -1,5 +1,5 @@
 //
-//  RootViewController.swift
+//  UnlockViewController.swift
 //  Unlock
 //
 //  Created by KimSe young on 2/5/18.
@@ -8,10 +8,74 @@
 
 import UIKit
 
-class RootViewController: UIViewController, UIPageViewControllerDelegate {
+class UnlockViewController: UIViewController {
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBOutlet weak var startPoint: UIImageView!
+    @IBOutlet weak var firstPoint: UIImageView!
+    @IBOutlet weak var secondPoint: UIImageView!
+    @IBOutlet weak var endPoint: UIImageView!
 
-    var pageViewController: UIPageViewController?
+    ////////////UNLOCK SEQUENCE//////////////
+    var swipePattern = [Int]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Reset screen on app start.
+        resetScreen()
+    }
+    
+    func resetScreen(){
+        // Initialize status label
+        statusLabel.text = "Find the fastest way home from school"
+        
+        // Update visual feedback
+        updateCircleFeedback(circleNumber: 1, isOn: false)
+        updateCircleFeedback(circleNumber: 2, isOn: false)
+        updateCircleFeedback(circleNumber: 3, isOn: false)
+        (circleNumber: 4, isOn: false)
 
+        // Flush input pattern.
+        swipePattern.removeAll()
+    }
+    
+    // Helper method to update circle alpha for visual feedback. Don't need this in the future.
+    func updateCircleFeedback(circleNumber: Int, isOn: Bool){
+        if circleNumber == 1 {
+            startPoint.alpha = isOn ? 0.2 : 1.0
+            firstPoint.alpha = isOn ? 0.2 : 1.0
+            secondPoint.alpha = isOn ? 0.2 : 1.0
+            endPoint.alpha = isOn ? 0.2 : 1.0
+        }
+    }
+    
+    // Override touchesMoved method already provided by parent class
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?){
+        // Get first touch.
+        if let firstTouch = touches.first{
+            //Check which view user is touching.
+            let hitView = self.view.hitTest(firstTouch.location(in: self.view), with: event)
+            
+            // Process swipe pattern based on which circle is touched.
+            if hitView == startPoint {
+                processSwipePattern(circleNumber: 1)
+            } else if hitView == firstPoint {
+                processSwipePattern(circleNumber: 2)
+            } else if hitView == secondPoint {
+                processingSwipePatter(circleNumber: 3)
+            } else if hitView == endPoint {
+                processingSwipePattern(circleumber: 4)
+            }
+        }
+    }
+    
+    // Override touchesEded method already provided by parent class.
+    override func touchesEnded(_ touches: Set<UITouch>, with evet: UIEvent?){
+        // Reset the scree when user lifts touch off.
+        resetScreen()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
